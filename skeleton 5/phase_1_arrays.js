f8986942f2bb3bae19750a17b8406eba7385c383
jackcusick95 <jackcusick95@gmail.com>
2021-04-14 14:23:27 -0400

    Did first two questions

--- skeleton 5/phase_1_arrays.js
+++ skeleton 5/phase_1_arrays.js
@@ -1,12 +1,24 @@
 
-Array.prototype.uniq (array) {
+Array.prototype.uniq = function () {
     let answer = [];
-    for (let i = 0, i < array.length, i++) {
-        if !answer.includes(array[i]) {
-            answer.push(array[i]);
+    for (let i = 0; i < this.length; i++) {
+        if (!answer.includes(this[i])) {
+            answer.push(this[i]);
         }
     }
-    return answer; 
+    console.log(answer); 
 };
 
-console.log([1,2,2,3,3,3.uniq()])
+// [1, 2, 2, 3, 3, 3].uniq()
+
+Array.prototype.twoSum = function () {
+    let sum = [];
+    for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i+1; j < this.length; j++) {
+            if (this[i] + this[j] == 0) {
+                sum.push([i, j]);
+            }
+        }
+    }
+    console.log(sum);
+};
